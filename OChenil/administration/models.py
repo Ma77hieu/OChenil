3944b3d1facae5d94451b9108207ec9ae31d5511
Matthieu O <mat.ortlieb@gmail.com>
2021-08-09 11:38:58 +0200

    database modification: adding a dedicated size table

--- OChenil/administration/models.py
+++ OChenil/administration/models.py
@@ -4,10 +4,17 @@ from authentication.models import User
 # Create your models here.
 
 
+class Size(models.Model):
+    size_name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.size_name
+
+
 class Dog(models.Model):
     dog_name = models.CharField(max_length=45)
     dog_age = models.IntegerField()
-    dog_size = models.CharField(max_length=10)
+    dogsize = models.ForeignKey(Size, on_delete=models.CASCADE, default=2)
     dog_race = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -16,7 +23,16 @@ class Dog(models.Model):
 
 
 class Box(models.Model):
-    allowed_size = models.CharField(max_length=255)
+    box_size = models.ForeignKey(Size, on_delete=models.CASCADE, default=2)
+
+    def __str__(self):
+        return self.pk
+
+
+class Unavailability(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    box = models.ForeignKey(Box, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.pk
@@ -29,6 +45,7 @@ class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
     box = models.ForeignKey(Box, on_delete=models.CASCADE)
+    booking_size = models.ForeignKey(Size, on_delete=models.CASCADE, default=2)
 
     def __str__(self):
         return self.pk
