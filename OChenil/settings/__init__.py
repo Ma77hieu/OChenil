ca99b7cf33263c23885c363798ef26318aab0f20
Matthieu O <mat.ortlieb@gmail.com>
2021-08-25 01:39:47 +0200

    corrected issue with the basedir path of the django settings (and static file path as well)

--- OChenil/settings/__init__.py
+++ OChenil/settings/__init__.py
@@ -15,7 +15,7 @@ import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -141,7 +141,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'OChenil/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
