71fcf2ae1f800068b35733357265071ace2ffea5
Matthieu O <mat.ortlieb@gmail.com>
2021-08-25 02:27:54 +0200

    new way of writing test_add_booking_not_ok

--- customer/tests/tests_functionnals.py
+++ customer/tests/tests_functionnals.py
@@ -118,16 +118,25 @@ class MySeleniumTests(StaticLiveServerTestCase):
             "booking_btn_in_page")
         booking_btn_in_page.send_keys(Keys.RETURN)
         ensure_change_page(self)
-        booking_added = True
-        custom_log("NO_AVAILABILITY in self.selenium.page_source",
-                   (NO_AVAILABILITY in self.selenium.page_source))
-        custom_log("Aug. 16, 2021 not in self.selenium.page_source",
-                   ("Aug. 16, 2021" not in self.selenium.page_source))
-        if (NO_AVAILABILITY in self.selenium.page_source) and (
-            "Aug. 16, 2021" not in self.selenium.page_source
-        ):
-            booking_added = False
-        assert booking_added is False
+        booking_not_added = False
+        # custom_log("NO_AVAILABILITY in self.selenium.page_source",
+        #            (NO_AVAILABILITY in self.selenium.page_source))
+        # custom_log("Aug. 16, 2021 not in self.selenium.page_source",
+        #            ("Aug. 16, 2021" not in self.selenium.page_source))
+        negative_user_feedback_displayed = (
+            NO_AVAILABILITY in self.selenium.page_source)
+        new_booking_not_displayed = (
+            ("Aug. 16, 2021") not in self.selenium.page_source)
+        custom_log("negative_user_feedback_displayed",
+                   negative_user_feedback_displayed)
+        custom_log("new_booking_not_displayed", new_booking_not_displayed)
+        if negative_user_feedback_displayed and new_booking_not_displayed:
+            booking_not_added = True
+        assert booking_not_added is True
+
+        # if (NO_AVAILABILITY in self.selenium.page_source) and ("Aug. 16, 2021" not in self.selenium.page_source):
+        #     booking_not_added = True
+        # assert booking_not_added is True
 
     def check_navbar_collapsed(self):
         """check foor the presence of the collapsed navbar menu button,
